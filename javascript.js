5c96d896e43035b7daa069dcfb606f287ebe1d6a
Jack Cohen <jmcohen99@gmail.com>
2024-10-19 20:13:01 -0500

    Created draw effect, clear button, create standard grid button, create custom grid button

--- javascript.js
+++ javascript.js
@@ -1,4 +1,3 @@
-
 // Grid Creation
 
 const standardGridRowCount = 16
@@ -6,23 +5,33 @@ const standardRowSquareCount = 16
 var userInputGridRowCount = 0
 var userInputGridSquareCount = 0
 
+gridSquares = document.getElementsByClassName("individualSquare")
+gridContainer = document.getElementById("grid-container")
 
 function createGrid(gridSize) {
     for (i=1; i <= gridSize; i++) { 
         grid = document.getElementById('grid-container')
-        gridRow = createRow(i)
+        gridRow = createRow(i, gridSize) // gridSize equals how many squares should be in a row
         grid.appendChild(gridRow)
-}}
+    }
+    console.log("Grid created")
+    for (i=0; i < gridSquares.length; i++) { 
+        gridSquares[i].addEventListener('mouseover', (changeColor) => {
+            changeColor.target.style.background = "black"
+        })
+    }
+    console.log("Hover event listener set")
+}
 
 function createSquare() {
     let individualSquare = Object.assign(document.createElement("div"), { className: 'individualSquare' })
     return individualSquare
 }
 
-function createRow(rowCount) {
+function createRow(rowCount, squareCount) {
     let rowID = "row-" + rowCount 
     let gridRow = Object.assign(document.createElement("div"), { id: rowID }, {className: "grid-row"} )
-    for (j=1; j <= standardRowSquareCount; j++) {
+    for (j=1; j <= squareCount; j++) {
         individualSquare = createSquare()
         squareID = "square-"+String(rowCount)+"-"+String(j)
         individualSquare.id = squareID
@@ -31,6 +40,29 @@ function createRow(rowCount) {
     return gridRow  
 }
 
-createGrid(standardGridRowCount)
+createGrid(standardGridRowCount) // Creating Grid
 
-// Event Listeners/Animation
+// Create custom grid
+newCustomGridButton = document.getElementById("new-custom-grid-button")
+newCustomGridButton.addEventListener("click", (makeCustomGrid) => {
+    userInputGridRowCount = prompt("How large should the square grid be? (Pick a number 1-100)")
+    userInputGridSquareCount = userInputGridRowCount
+    gridContainer.innerHTML = ""
+    createGrid(userInputGridRowCount)
+})
+
+// Clear button
+clearButton = document.getElementById("clear-button")
+clearButton.addEventListener("click", (clearSquare) => {
+    for (i=0; i < gridSquares.length; i++) { 
+        gridSquares[i].style.background = "white"
+    }
+    console.log("Grid cleared")
+}) 
+
+// Create fresh standard grid
+newStandardGridButton = document.getElementById("new-standard-grid-button")
+newStandardGridButton.addEventListener("click", (makeStandardGrid) => {
+    gridContainer.innerHTML = ""
+    createGrid(standardGridRowCount)
+})
